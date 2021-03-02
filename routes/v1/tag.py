03a5761429eaa27ed4ad970d14f06ace8c8e8ccb
I518703 <sreevishnu.a.b@sap.com>
2021-03-02 14:58:08 +0530

    adding GET tags endpoint

--- routes/v1/tag.py
+++ routes/v1/tag.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 import yake
+from sqlalchemy.sql import select
 
 from config.db import DB
 from utils.logger import get_logger
@@ -22,3 +23,11 @@ async def create_tag(item: Item):
   await db.execute_many(query=query, values=top_tags)
   LOGGER.error(top_tags)
   return item
+
+@router.get("/{id}")
+async def get_tags(id):
+  db = DB.get_instance().database
+  query = select([tag]).where(tag.c.id == id)
+  result = await db.fetch_all(query)
+  LOGGER.error(result)
+  return result
